e42a304399c0ff69acf0200950aaf38a0f928c8b
Martin Krecek <martinkrecek98@seznam.cz>
2021-03-06 19:19:56 +0100

    Simple streamlit application

--- streamlit_deployment.py
+++ streamlit_deployment.py
@@ -1,1 +1,53 @@
 # https://towardsdatascience.com/quickly-build-and-deploy-an-application-with-streamlit-988ca08c7e83
+
+'''
+import pandas as pd
+import numpy as np
+import altair as alt
+import streamlit as st
+
+df = pd.DataFrame(np.random.randn(200, 3), columns=['a', 'b', 'c'])
+c = alt.Chart(df).mark_circle().encode(x='a', y='b', size='c',  
+                                       color='c')
+st.altair_chart(c)
+
+
+df = pd.read_csv("https://github.com/MaartenGr/boardgame/raw/master/files/boardgame_new.csv").head()
+st.write(df)
+'''
+
+from vega_datasets import data
+import streamlit as st
+import altair as alt
+
+def main():
+    df = load_data()
+    page = st.sidebar.selectbox("Choose a page", ["Homepage", "Exploration"])
+
+    if page == "Homepage":
+        st.header("This is your data explorer.")
+        st.write("Please select a page on the left.")
+        st.write(df)
+    elif page == "Exploration":
+        st.title("Data Exploration")
+        x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, index=3)
+        y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, index=4)
+        visualize_data(df, x_axis, y_axis)
+
+@st.cache
+def load_data():
+    df = data.cars()
+    return df
+
+def visualize_data(df, x_axis, y_axis):
+    graph = alt.Chart(df).mark_circle(size=60).encode(
+        x=x_axis,
+        y=y_axis,
+        color='Origin',
+        tooltip=['Name', 'Origin', 'Horsepower', 'Miles_per_Gallon']
+    ).interactive()
+
+    st.write(graph)
+
+if __name__ == "__main__":
+    main()
